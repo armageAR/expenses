071c4ae1b83d8e7d14d41ddcc39d4c9352fd309e
armageAR <armage@gmail.com>
2020-05-09 12:35:15 -0400

    Documents API index

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Document;
 
 class DocumentController extends Controller
 {
@@ -14,7 +15,7 @@ class DocumentController extends Controller
      */
     public function index()
     {
-        //
+        return Document::all();
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('documents', 'DocumentController', ['as' => 'api']);
+Route::resource('documents', 'Api\DocumentController', ['as' => 'api']);
